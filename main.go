2fa058614a25bde986bdc2def98711da2f25b02d
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-05-16 22:44:51 -0300

    Exibindo resultado

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-	"fmt"
-
-	db2 "github.com/Daniel-Vinicius/go-ports-adapters/adapters/db"
-	"github.com/Daniel-Vinicius/go-ports-adapters/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/Daniel-Vinicius/go-ports-adapters/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductPersistenceSQLite(db)
-
-	productService := application.NewProductService(productDbAdapter)
-
-	productCreated, _ := productService.Create("Shirt", 30)
-
-	productService.Enable(productCreated)
-
-	fmt.Print(productCreated)
+	cmd.Execute()
 }
